7654a1e29291a157290a15b31b76847d96c9f292
Nikita Phapale <113188061+NikitaPhapale@users.noreply.github.com>
2025-04-09 00:48:15 +0530

    encapsulation example

--- E8/src/PrincipleOfOops/Student.java
+++ E8/src/PrincipleOfOops/Student.java
@@ -1,5 +1,36 @@
 package PrincipleOfOops;
 
 public class Student {
+	private String name;
+	private String clgName;
+	private String course;
+	private int age;
+	
+	Student(String name,String college,String course,int age)
+	{
+		this.name=name;
+		clgName=college;
+		this.course=course;
+		this.age=age;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
 
+	public String getClg()
+	{
+		return clgName;
+	}
+	public String getCourse()
+	{
+		return course;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+	
 }
+
---
+++ E8/src/PrincipleOfOops/StudentDriver.java
@@ -0,0 +1,15 @@
+package PrincipleOfOops;
+
+public class StudentDriver {
+	
+		public static void main(String args[])
+		{
+	  Student s1=new Student("Nikita","SITRC","BE Computer",20);
+	  
+	  System.out.println(s1.getName());
+	  System.out.println(s1.getClg());
+	  System.out.println(s1.getCourse());
+	  System.out.println(s1.getAge());
+		}
+
+}
